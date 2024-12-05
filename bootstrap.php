fbccbe71f66c1a700056b50a421dd03b5327513d
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 21:17:17 +0630

    dev :  workflow-test

--- bootstrap.php
+++ bootstrap.php
@@ -8,8 +8,15 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}else{
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
 
 return require CONFIG_PATH . '/container/container.php';
 

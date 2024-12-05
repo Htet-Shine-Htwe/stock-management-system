affcecf1e7a6bdb36cbfdb4d94549c902d90ef28
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 21:35:30 +0630

    dev : config

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use App\Enum\AppEnvironment;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 
@@ -9,7 +10,7 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
 
 
-if ($_ENV['APP_ENV'] === 'production') {
+if ($_ENV['APP_ENV'] == AppEnvironment::Production->value){
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }else{

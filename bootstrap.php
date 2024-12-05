943e74fce798fabbb585ce9aff9c4da6742c6091
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 21:29:44 +0630

    dev : test

--- bootstrap.php
+++ bootstrap.php
@@ -9,14 +9,13 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
 
 
-if (file_exists(__DIR__ . '/../.env')) {
+if ($_ENV['APP_ENV'] === 'production') {
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }else{
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
-
 
 return require CONFIG_PATH . '/container/container.php';
 

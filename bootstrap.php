405d2cff95f58503040f488fce31db7c2a694a91
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 21:46:50 +0630

    dev : rebuild

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
     
 
-$env_path = file_exists(__DIR__ . '/../.env') ? __DIR__ . '/../' : __DIR__;
+$env_path = file_exists(__DIR__ . '/../.env') ? __DIR__ . `/../` : __DIR__;
 
 $dotenv = Dotenv::createImmutable($env_path);
 $dotenv->load();

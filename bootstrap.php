e97ac4b37e6318403447079195981de86aa9f6bf
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 20:44:59 +0630

    Add intl extension requirement

--- bootstrap.php
+++ bootstrap.php
@@ -8,10 +8,9 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
 
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
-}
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 return require CONFIG_PATH . '/container/container.php';
 
 

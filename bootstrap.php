e6bc678c48c923bab49b385bee02eb91090998f5
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-05 21:43:21 +0630

    dev : test rebuild

--- bootstrap.php
+++ bootstrap.php
@@ -7,9 +7,9 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/configs/path_constants.php';
+    
 
-
-$env_path = file_exists(__DIR__ . '/../.env') ? __DIR__ . '/../.env' : __DIR__;
+$env_path = file_exists(__DIR__ . '/../.env') ? __DIR__ . '/../' : __DIR__;
 
 $dotenv = Dotenv::createImmutable($env_path);
 $dotenv->load();

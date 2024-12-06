7e94b5a9cc6a171048c5c6caa2e30b92654111be
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-12-06 12:49:23 +0630

    style : add login and alert css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,20 @@
 import "../css/app.scss"
 
 require('bootstrap')
+
+
+function showNotification(message, type) {
+    const notification = document.createElement('div');
+    notification.classList.add('notification', type);
+    notification.textContent = message;
+
+    document.body.appendChild(notification);
+
+    // Auto-remove notification after 3 seconds
+    setTimeout(() => {
+        notification.remove();
+    }, 3000);
+}  
+
+
+window.showNotification = showNotification;
